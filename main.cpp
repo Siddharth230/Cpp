601e23484ff4b8597303b52c04e51f5e98bbeca5
Siddharth230 <siddharthlokhande04@gmail.com>
2025-09-17 11:56:32 +0530

    Useful math related functions

--- main.cpp
+++ main.cpp
@@ -1,26 +1,23 @@
+#include <cmath>
 #include <iostream>
 
-//* cout << (Insertion Operator)
-//* cin << (Extraction Operator)
-
 int main() {
-  std::string name;
-  std::string fullName;
-  int age;
+  double x = 3;
+  double y = 4;
+  double pi = 3.14;
+  double q = 3.99;
+  double z;
 
-  std::cout << "What's your name?: ";
-  std::cin >> name;
+  // z = std::max(x, y);  // 4
+  // z = std::min(x, y);  // 3
+  // z = pow(2, 3);  // 2*2*2 = 8
+  // z = sqrt(9);  // 3
+  // z = abs(-5);  // 5
+  // z = round(pi);  // 3
+  // z = ceil(pi);  // 4
+  z = floor(q);  // 3
 
-  std::cin.ignore();  //* Discard the newline character
-
-  std::cout << "What's your full name?: ";
-  std::getline(std::cin, fullName);
-
-  std::cout << "What's your age?: ";
-  std::cin >> age;
-
-  std::cout << "Hello " << name << std::endl;
-  std::cout << "You are " << age << " years old.";
+  std::cout << z;
 
   return 0;
 }
