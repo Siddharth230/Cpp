0b412d436600686f8f6be72dd4a916c7705eb317
Siddharth230 <siddharthlokhande04@gmail.com>
2025-09-17 13:00:46 +0530

    Switches

--- main.cpp
+++ main.cpp
@@ -1,22 +1,86 @@
 #include <iostream>
 
 int main() {
-  //* if statements = Do something if a condition is true.
-  //*                 if not, then don't do it.
+  //* switch = alternative to using many "else if" statements
+  //*          compare one value against matching cases
 
-  int age;
+  int month;
+  std::cout << "Enter the month (1-12): ";
+  std::cin >> month;
 
-  std::cout << "Enter your age: ";
-  std::cin >> age;
+  switch (month) {
+    case 1:
+      std::cout << "It is January";
+      break;
+    case 2:
+      std::cout << "It is February";
+      break;
+    case 3:
+      std::cout << "It is March";
+      break;
+    case 4:
+      std::cout << "It is April";
+      break;
+    case 5:
+      std::cout << "It is May";
+      break;
+    case 6:
+      std::cout << "It is June";
+      break;
+    case 7:
+      std::cout << "It is July";
+      break;
+    case 8:
+      std::cout << "It is August";
+      break;
+    case 9:
+      std::cout << "It is September";
+      break;
+    case 10:
+      std::cout << "It is October";
+      break;
+    case 11:
+      std::cout << "It is November";
+      break;
+    case 12:
+      std::cout << "It is December";
+      break;
+    default:
+      std::cout << "Please enter in only number (1-12)";
+  }
 
-  if (age >= 100) {
-    std::cout << "You should be Dead";
-  } else if (age >= 18) {
-    std::cout << "Welcome to the site";
-  } else if (age < 0) {
-    std::cout << "Enter valid age";
-  } else {
-    std::cout << "You are underage.";
+  char grade;
+
+  std::cout << "What Grade?: ";
+  std::cin >> grade;
+
+  switch (grade) {
+    case 'A':
+    case 'a':
+      std::cout << "You did great!";
+      break;
+    case 'B':
+    case 'b':
+      std::cout << "You did good";
+      break;
+    case 'C':
+    case 'c':
+      std::cout << "You did okay";
+      break;
+    case 'D':
+    case 'd':
+      std::cout << "You did not do good";
+      break;
+    case 'E':
+    case 'e':
+      std::cout << "You did very bad";
+      break;
+    case 'F':
+    case 'f':
+      std::cout << "You failed";
+      break;
+    default:
+      std::cout << "Please Enter Valid Grade(A-F).";
   }
 
   return 0;
