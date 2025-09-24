62af8c3d8e6086410b4cde656ade3cd88680a91f
Siddharth230 <siddharthlokhande04@gmail.com>
2025-09-24 16:50:11 +0530

    Foreach Loop

---
+++ 35. Foreach Loop/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+  //* Foreach Loop = Loop that eases that traversal over
+  //*                an iterable data set
+
+  std::string students[] = {"Spongebob", "Patrick", "Squidward"};
+  int grades[] = {65, 72, 81, 93};
+
+  for (std::string student : students) {
+    std::cout << student << '\n';
+  }
+
+  for (int grade : grades) {
+    std::cout << grade << '\n';
+  }
+
+  return 0;
+}
